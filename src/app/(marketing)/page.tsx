ee726d821d7cfd12ebbab838dabf079fbd6a98c1
Yazi27 <yannbricage@pm.me>
2025-01-26 00:34:55 +0100

    Routing Structure)

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <main className="bg-[#5DC9A8] min-h-screen">
+      <Image src="" />
+    </main>
+  );
+}
