d52a569b840ba24b761243c9c3203d6c88dcfd0d
Yazi27 <yannbricage@pm.me>
2025-01-26 01:32:49 +0100

    Home Page + Shadcn customizations + Accesibility (Anchors behaving like buttons)

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,10 +1,39 @@
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function HomePage() {
   return (
-    <main className="bg-[#5DC9A8] min-h-screen">
-      <Image src="" />
+    <main className="bg-[#5DC9A8] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        alt="Preview of Petsoft"
+        width={519}
+        height={472}
+      />
+
+      <section>
+        <Logo />
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+          Manage your <span className="font-extrabold">pet daycare</span> with
+          ease
+        </h1>
+        <p className="text-2xl font-medium max-w-[600px]">
+          Use PetSoft to easily keep track of pets under your care. Get lifetime
+          access for $299.
+        </p>
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href="/app">Get started</Link>
+          </Button>
+
+          <Button asChild variant="secondary">
+            <Link href="/signup">Log in</Link>
+          </Button>
+        </div>
+      </section>
     </main>
   );
 }
