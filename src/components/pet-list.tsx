342473a3f245c0be28a137ea7cd4fcb8b839e20c
Yazi27 <yannbricage@pm.me>
2025-01-28 13:45:39 +0100

    Conditional rendering for selected pet id with cn

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -2,20 +2,27 @@
 
 import { usePetContext } from "@/lib/hooks";
 import { TPetData } from "@/lib/types";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
 export default function PetList() {
-  const { pets } = usePetContext();
+  const { pets, handleChangeSelectPetId, selectedPetId } = usePetContext();
 
   console.log(pets);
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       <li>
-        {pets.map((pet: TPetData) => (
+        {pets.map((pet) => (
           <button
+            onClick={() => handleChangeSelectPetId(pet.id)}
             key={pet.id}
-            className="flex h-[70px] w-full items-center px-5 text-base gap-3 cursor-pointer hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition"
+            className={cn(
+              "flex h-[70px] w-full items-center px-5 text-base gap-3 cursor-pointer hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition",
+              {
+                "bg-[#EFF1F2]": selectedPetId === pet.id,
+              }
+            )}
           >
             <Image
               src={pet.imageUrl}
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,5 @@
 export type TPetData = {
-  id: number;
+  id: string;
   name: string;
   ownerName: string;
   imageUrl: string;
